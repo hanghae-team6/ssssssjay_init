1b8adf4bb871c0f1ceba1be01c65953bdff4223c
Sungjae.Kwak <sungjae8970@gmail.com>
2024-04-10 01:01:48 +0900

    chore: formatting fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   ignorePatterns: [
     'dist',
     '.eslintrc.cjs',
